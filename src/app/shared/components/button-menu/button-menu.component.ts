52b6a76b3613053c2fc12c409b84106c4d659d8b
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-12 17:10:01 -0300

    feat: criado o button menu

---
+++ src/app/shared/components/button-menu/button-menu.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+	selector: 'app-button-menu',
+	standalone: true,
+	imports: [],
+	templateUrl: './button-menu.component.html',
+	styleUrl: './button-menu.component.scss',
+})
+export class ButtonMenuComponent implements OnInit {
+	@Input() description = '';
+	@Input() selected = false;
+	@Output() click = new EventEmitter<void>();
+
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	onClick() {
+		this.click.emit();
+	}
+}
