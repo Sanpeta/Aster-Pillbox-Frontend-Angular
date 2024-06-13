bad6545c69a63639ecfa79aabf2eda50e92c1261
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-13 18:45:39 -0300

    feat: adicionado mais imagens e ajustado a toolbar

--- src/app/shared/components/toolbar-dashboard/toolbar-dashboard.component.ts
+++ src/app/shared/components/toolbar-dashboard/toolbar-dashboard.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
 	selector: 'app-toolbar-dashboard',
 	standalone: true,
-	imports: [],
+	imports: [IconComponent],
 	templateUrl: './toolbar-dashboard.component.html',
 	styleUrl: './toolbar-dashboard.component.scss',
 })
 export class ToolbarDashboardComponent {
 	isDarkTheme = false;
+	srcImage = 'assets/images/light-mode-icon.svg';
 
 	constructor() {
 		// this.isDarkTheme = this.getTheme();
@@ -16,6 +18,11 @@ export class ToolbarDashboardComponent {
 
 	toggleTheme() {
 		this.isDarkTheme = !this.isDarkTheme;
+		if (this.isDarkTheme) {
+			this.srcImage = 'assets/images/dark-mode-icon.svg';
+		} else {
+			this.srcImage = 'assets/images/light-mode-icon.svg';
+		}
 		// this.setTheme(this.isDarkTheme);
 	}
 }
