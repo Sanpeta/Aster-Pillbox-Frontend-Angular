c656cf15f0f2ddefa8260ca8ee6b2146cd5dcaa1
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-12 22:55:38 -0300

    refactor: criado um layout para o dashboard, pois vou esta aproveitando os componentes shareds.

---
+++ src/app/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { SidenavDashboardComponent } from '../../shared/components/sidenav-dashboard/sidenav-dashboard.component';
+import { SidenavTitleDashboardComponent } from '../../shared/components/sidenav-title-dashboard/sidenav-title-dashboard.component';
+import { ToolbarDashboardComponent } from '../../shared/components/toolbar-dashboard/toolbar-dashboard.component';
+
+@Component({
+	selector: 'app-dashboard-layout',
+	standalone: true,
+	imports: [
+		ToolbarDashboardComponent,
+		SidenavDashboardComponent,
+		SidenavTitleDashboardComponent,
+	],
+	templateUrl: './dashboard-layout.component.html',
+	styleUrl: './dashboard-layout.component.scss',
+})
+export class DashboardLayoutComponent {}
