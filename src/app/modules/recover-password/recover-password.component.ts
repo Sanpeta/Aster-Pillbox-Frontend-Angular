af9af012bfeb4e42c1dbf1f883e36c8ccac59c57
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-13 23:11:37 -0300

    feat: criado page de recover-password e ja adicionado na rota.

---
+++ src/app/modules/recover-password/recover-password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IconComponent } from '../../shared/components/icon/icon.component';
+
+@Component({
+	selector: 'app-recover-password',
+	standalone: true,
+	imports: [IconComponent],
+	templateUrl: './recover-password.component.html',
+	styleUrl: './recover-password.component.css',
+})
+export class RecoverPasswordComponent {}
