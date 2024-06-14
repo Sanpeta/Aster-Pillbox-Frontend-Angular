e5c877db653861abb9f311cd55009e9b207faf30
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-13 23:12:04 -0300

    feat: adicionado a rota do recover-password e adiciondo o icone novo.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { LoginComponent } from './modules/login/login.component';
+import { RecoverPasswordComponent } from './modules/recover-password/recover-password.component';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 	{ path: 'dashboard', component: DashboardComponent },
 	{ path: 'login', component: LoginComponent },
+	{ path: 'recover-password', component: RecoverPasswordComponent },
 ];
