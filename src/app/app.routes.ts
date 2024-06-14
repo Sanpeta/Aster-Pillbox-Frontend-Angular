b747bbd2ba70fd232343f852c32a635d9f1c4895
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-13 23:44:33 -0300

    feat: criado page de register e ja adicionado na rota.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { LoginComponent } from './modules/login/login.component';
 import { RecoverPasswordComponent } from './modules/recover-password/recover-password.component';
+import { RegisterComponent } from './modules/register/register.component';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 	{ path: 'dashboard', component: DashboardComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'recover-password', component: RecoverPasswordComponent },
+	{ path: 'register', component: RegisterComponent },
 ];
---
+++ src/app/modules/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IconComponent } from '../../shared/components/icon/icon.component';
+
+@Component({
+	selector: 'app-register',
+	standalone: true,
+	imports: [IconComponent],
+	templateUrl: './register.component.html',
+	styleUrl: './register.component.css',
+})
+export class RegisterComponent {}
