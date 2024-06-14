6d380696df5961b336f5ef33e160a6397adb387b
Sidnei de Souza Junior <sidneidesouzajunior@192.168.1.8>
2024-06-14 17:22:57 -0300

    feat: adicionado o routerModule para utilziar no html o routerlink.

--- src/app/modules/recover-password/recover-password.component.ts
+++ src/app/modules/recover-password/recover-password.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IconComponent } from '../../shared/components/icon/icon.component';
 
 @Component({
 	selector: 'app-recover-password',
 	standalone: true,
-	imports: [IconComponent],
+	imports: [IconComponent, RouterModule],
 	templateUrl: './recover-password.component.html',
 	styleUrl: './recover-password.component.css',
 })
