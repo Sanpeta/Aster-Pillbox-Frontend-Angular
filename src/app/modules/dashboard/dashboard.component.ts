9e0b7e6b95d49973ebf97a7656fdff40f32d2038
Sidnei de Souza Junior <sidneidesouzajunior@192.168.1.8>
2024-06-17 14:50:30 -0300

    feat: adicionado o componente de toast no dashboard, mais ainda precisa fazer as configuracoes necessarias.

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,12 +1,47 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+	Component,
+	ComponentFactoryResolver,
+	ViewChild,
+	ViewContainerRef,
+} from '@angular/core';
 import { DashboardLayoutComponent } from '../../layouts/dashboard-layout/dashboard-layout.component';
+import { ToastComponent } from '../../shared/components/toast/toast.component';
 
 @Component({
 	selector: 'app-dashboard',
 	templateUrl: './dashboard.component.html',
 	styleUrl: './dashboard.component.scss',
 	standalone: true,
-	imports: [AsyncPipe, DashboardLayoutComponent],
+	imports: [AsyncPipe, DashboardLayoutComponent, ToastComponent],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+	@ViewChild('toastContainer', { read: ViewContainerRef })
+	toast!: ViewContainerRef;
+
+	constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+
+	ngOnInit() {}
+
+	ngAfterViewInit() {
+		// Use ngAfterViewInit instead of ngOnInit
+		this.showToast('Bem-vindo', 'success');
+	}
+
+	showToast(
+		mensagem: string,
+		type: 'success' | 'error' | 'info' | 'warning' = 'success'
+	) {
+		const componentFactory =
+			this.componentFactoryResolver.resolveComponentFactory(
+				ToastComponent
+			);
+		this.toast.clear(); // Limpa o container antes de criar um novo toast
+		const componentRef = this.toast.createComponent(componentFactory);
+		componentRef.instance.mensage = mensagem;
+		componentRef.instance.type = type;
+		componentRef.instance.closeToast.subscribe(() => {
+			componentRef.destroy();
+		});
+	}
+}
