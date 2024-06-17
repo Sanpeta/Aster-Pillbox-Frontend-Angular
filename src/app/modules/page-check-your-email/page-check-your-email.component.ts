fd5bdc97188b1ca2ebe9d1e58a5a7bcf0118f358
Sidnei de Souza Junior <sidneidesouzajunior@192.168.1.8>
2024-06-17 18:50:20 -0300

    feat: criado uma condicao de verificar se foi encaminhado o email se caso for nao vai aparecer o button de reenviar. Caso precisar reenviar, vai aparecer o button.

--- src/app/modules/page-check-your-email/page-check-your-email.component.ts
+++ src/app/modules/page-check-your-email/page-check-your-email.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Subject, takeUntil } from 'rxjs';
+import { AccountService } from '../../services/account/account.service';
+import { LoaderComponent } from '../../shared/components/loader/loader.component';
 
 @Component({
 	selector: 'app-page-check-your-email',
 	standalone: true,
-	imports: [RouterModule],
+	imports: [RouterModule, LoaderComponent],
 	templateUrl: './page-check-your-email.component.html',
 	styleUrl: './page-check-your-email.component.css',
 })
-export class PageCheckYourEmailComponent {}
+export class PageCheckYourEmailComponent {
+	private destroy$ = new Subject<void>();
+	public loader = false;
+	public title = 'Conta Ativada';
+	public description = 'Só falta entrar e começar a usar!';
+	public sendedEmail = false;
+	public showResendButton = false;
+	private emailAccount: string | null = null;
+
+	constructor(
+		private accountActivation: AccountService,
+		private cookieService: CookieService
+	) {}
+
+	ngOnInit() {
+		this.emailAccount = this.cookieService.get('ACCOUNT_EMAIL');
+		if (this.emailAccount) {
+			this.showResendButton = true;
+		}
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
+	resendEmail() {
+		this.loader = true;
+		this.accountActivation
+			.createTokenAccountActivate(this.emailAccount!)
+			.pipe(takeUntil(this.destroy$))
+			.subscribe({
+				next: (response) => {
+					console.log(response);
+					this.loader = false;
+					this.sendedEmail = true;
+				},
+				error: (err) => {
+					console.log(err.error);
+					this.loader = false;
+					this.sendedEmail = false;
+				},
+			});
+	}
+}
