af48652e8d83843cd71f6f590f9e995c31bb0cc0
Sidnei de Souza Junior <sidneidesouzajunior@192.168.1.8>
2024-06-18 12:50:29 -0300

    fix: utilizando na pagina loader e dialog para ter interacao com o usuario.

--- src/app/modules/page-confirm-recover-password/page-confirm-recover-password.component.ts
+++ src/app/modules/page-confirm-recover-password/page-confirm-recover-password.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { Subject, takeUntil } from 'rxjs';
 import { UpdateAccountResetPasswordRequest } from '../../models/interfaces/account-reset-password/UpdateAccountResetPassword';
 import { AccountService } from '../../services/account/account.service';
+import { DialogComponent } from '../../shared/components/dialog/dialog.component';
 import { LoaderComponent } from '../../shared/components/loader/loader.component';
 
 @Component({
 	selector: 'app-page-confirm-recover-password',
 	standalone: true,
-	imports: [ReactiveFormsModule, RouterModule, LoaderComponent],
+	imports: [
+		ReactiveFormsModule,
+		RouterModule,
+		LoaderComponent,
+		DialogComponent,
+		LoaderComponent,
+	],
 	templateUrl: './page-confirm-recover-password.component.html',
 	styleUrl: './page-confirm-recover-password.component.css',
 })
 export class PageConfirmRecoverPasswordComponent {
 	private destroy$ = new Subject<void>();
-	public loader = true;
 	private token: string | null = null;
 	private accountID: string | null = null;
 	private emailAccount: string = '';
@@ -25,6 +31,10 @@ export class PageConfirmRecoverPasswordComponent {
 		account_id: 0,
 		password: '',
 	};
+	public showLoader = false;
+
+	@ViewChild('dialogContainer', { read: ViewContainerRef })
+	dialogContainer!: ViewContainerRef;
 
 	resetPasswordForm = this.formBuilder.group({
 		password: ['', [Validators.required, Validators.minLength(8)]],
@@ -50,7 +60,7 @@ export class PageConfirmRecoverPasswordComponent {
 		this.accountID = this.cookieService.get('ACCOUNT_ID');
 		this.emailAccount = this.cookieService.get('ACCOUNT_EMAIL');
 		if (!this.token || !this.accountID) {
-			this.loader = false;
+			this.showLoader = false;
 			return;
 		}
 		this.accUpdateResetPasswordRequest = {
@@ -66,6 +76,7 @@ export class PageConfirmRecoverPasswordComponent {
 	}
 
 	onSubmitResetPasswordForm(): void {
+		this.showLoader = true;
 		if (
 			this.resetPasswordForm.valid &&
 			this.resetPasswordForm.value.password ===
@@ -79,16 +90,58 @@ export class PageConfirmRecoverPasswordComponent {
 				.pipe(takeUntil(this.destroy$))
 				.subscribe({
 					next: (response) => {
-						this.loader = false;
+						this.showLoader = false;
 						console.log(response);
-						this.router.navigate(['/login']);
+						this.openDialog(
+							'Sucesso',
+							'Sua senha foi alterada com sucesso!',
+							'Continuar',
+							'',
+							() => {
+								this.router.navigate(['/login']);
+							}
+						);
 					},
 					error: (err) => {
-						this.loader = false;
+						this.showLoader = false;
 						console.log(err.error.code);
 						console.log(err.error);
+						this.openDialog(
+							'Erro',
+							'Não foi possível alterar sua senha! Token expirado ou inválido.',
+							'Continuar'
+						);
 					},
 				});
 		}
+	}
+
+	openDialog(
+		title: string,
+		mensage: string,
+		buttonTextConfirm: string,
+		buttonTextClose?: any | undefined,
+		funcConfirmButton?: any | null
+	): void {
+		const componentRef =
+			this.dialogContainer.createComponent(DialogComponent);
+
+		componentRef.instance.data = {
+			title: title,
+			mensage: mensage,
+			buttonTextConfirm: buttonTextConfirm,
+			buttonTextClose: buttonTextClose,
+		};
+
+		componentRef.instance.close.subscribe(() => {
+			this.dialogContainer.clear(); // Fecha o diálogo
+		});
+
+		componentRef.instance.confirm.subscribe(() => {
+			// Ação a ser executada se o usuário clicar em "Continuar"
+			if (funcConfirmButton) {
+				funcConfirmButton();
+			}
+		});
 	}
 }
