7e2f5d7d9193d9d423e17c13203d09a67d4a55ea
Sidnei de Souza Junior <sidneidesouzajunior@192.168.1.8>
2024-06-18 11:47:08 -0300

    fix: adicionado na pagina loader e dialog, mostrando se caso algo der de errado ou certo quando solicitado alteracao de senha.

--- src/app/modules/recover-password/recover-password.component.ts
+++ src/app/modules/recover-password/recover-password.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+	Component,
+	OnDestroy,
+	OnInit,
+	ViewChild,
+	ViewContainerRef,
+} from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { AccountService } from '../../services/account/account.service';
+import { DialogComponent } from '../../shared/components/dialog/dialog.component';
 import { IconComponent } from '../../shared/components/icon/icon.component';
+import { LoaderComponent } from '../../shared/components/loader/loader.component';
 
 @Component({
 	selector: 'app-recover-password',
 	standalone: true,
-	imports: [IconComponent, RouterModule, ReactiveFormsModule],
+	imports: [
+		IconComponent,
+		RouterModule,
+		ReactiveFormsModule,
+		LoaderComponent,
+		DialogComponent,
+	],
 	templateUrl: './recover-password.component.html',
 	styleUrl: './recover-password.component.css',
 })
 export class RecoverPasswordComponent implements OnInit, OnDestroy {
 	private destroy$ = new Subject<void>();
+	public showLoader = false;
+	@ViewChild('dialogContainer', { read: ViewContainerRef })
+	dialogContainer!: ViewContainerRef;
 
 	resetPassword = this.formBuilder.group({
 		email: ['', [Validators.required, Validators.email]],
@@ -34,6 +51,7 @@ export class RecoverPasswordComponent implements OnInit, OnDestroy {
 	}
 
 	onSubmitResetPassword(): void {
+		this.showLoader = true;
 		if (this.resetPassword.valid && this.resetPassword.value) {
 			this.accountService
 				.createTokenResetPassword(
@@ -42,12 +60,78 @@ export class RecoverPasswordComponent implements OnInit, OnDestroy {
 				.pipe(takeUntil(this.destroy$))
 				.subscribe({
 					next: (response) => {
-						console.log(response);
+						this.showLoader = false;
+						this.openDialog(
+							'Foi enviado um email',
+							'Favor verifique sua caixa de entrada e siga as instruções para redefinir sua senha.',
+							'Ok',
+							'',
+							() => {}
+						);
 					},
 					error: (error) => {
 						console.log(error);
+						this.showLoader = false;
+						switch (error.status) {
+							case 400:
+								this.openDialog(
+									'Ocorreu um erro',
+									'Favor verifique os dados informados e tente novamente.',
+									'Ok',
+									'Cancelar',
+									() => {}
+								);
+								break;
+							case 404:
+								this.openDialog(
+									'Ocorreu um erro',
+									'Favor verifique os dados informados e tente novamente.',
+									'Ok',
+									'Cancelar',
+									() => {}
+								);
+								break;
+							default:
+								this.openDialog(
+									'Ocorreu um erro',
+									'Favor verifique os dados informados e tente novamente.',
+									'Ok',
+									'Cancelar',
+									() => {}
+								);
+								break;
+						}
 					},
 				});
 		}
+	}
+
+	openDialog(
+		title: string,
+		mensage: string,
+		buttonTextConfirm: string,
+		buttonTextClose?: any | undefined,
+		funcConfirmButton?: any | null
+	): void {
+		const componentRef =
+			this.dialogContainer.createComponent(DialogComponent);
+
+		componentRef.instance.data = {
+			title: title,
+			mensage: mensage,
+			buttonTextConfirm: buttonTextConfirm,
+			buttonTextClose: buttonTextClose,
+		};
+
+		componentRef.instance.close.subscribe(() => {
+			this.dialogContainer.clear(); // Fecha o diálogo
+		});
+
+		componentRef.instance.confirm.subscribe(() => {
+			// Ação a ser executada se o usuário clicar em "Continuar"
+			if (funcConfirmButton) {
+				funcConfirmButton();
+			}
+		});
 	}
 }
