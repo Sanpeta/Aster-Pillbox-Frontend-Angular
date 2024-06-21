f1f42fe3f604144396bc77f49fd383008f488d4f
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-21 13:57:43 -0300

    feat: adicionado o module de route.

--- src/app/modules/dashboard/components/card-reminder/card-reminder.component.ts
+++ src/app/modules/dashboard/components/card-reminder/card-reminder.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IconComponent } from '../../../../shared/components/icon/icon.component';
 import { CardReminderItemComponent } from '../card-reminder-item/card-reminder-item.component';
 
 @Component({
 	selector: 'app-card-reminder',
 	standalone: true,
-	imports: [CardReminderItemComponent],
+	imports: [CardReminderItemComponent, IconComponent, RouterModule],
 	templateUrl: './card-reminder.component.html',
 	styleUrl: './card-reminder.component.css',
 })
