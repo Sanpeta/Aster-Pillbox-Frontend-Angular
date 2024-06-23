9303de19eb880d74eefd0b7c0760282cfdcab566
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-22 22:02:55 -0300

    feat: nesse primeiro momento foi criado a pagina de perfil do usuario.

--- src/app/modules/dashboard-perfil/dashboard-perfil.component.spec.ts
+++ src/app/modules/dashboard-perfil/dashboard-perfil.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardPerfilComponent } from './dashboard-perfil.component';
 
 describe('DashboardPerfilComponent', () => {
-  let component: DashboardPerfilComponent;
-  let fixture: ComponentFixture<DashboardPerfilComponent>;
+	let component: DashboardPerfilComponent;
+	let fixture: ComponentFixture<DashboardPerfilComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DashboardPerfilComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [DashboardPerfilComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(DashboardPerfilComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(DashboardPerfilComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/modules/dashboard-perfil/dashboard-perfil.component.ts
+++ src/app/modules/dashboard-perfil/dashboard-perfil.component.ts
@@ -1,5 +1,17 @@
-import { Component } from '@angular/core';
+import {
+	Component,
+	OnDestroy,
+	OnInit,
+	ViewChild,
+	ViewContainerRef,
+} from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Subject } from 'rxjs';
 import { DashboardLayoutComponent } from '../../layouts/dashboard-layout/dashboard-layout.component';
+import { DialogComponent } from '../../shared/components/dialog/dialog.component';
+import { FormInputComponent } from '../../shared/components/form-input/form-input.component';
 import { SidenavDashboardComponent } from '../../shared/components/sidenav-dashboard/sidenav-dashboard.component';
 import { SidenavTitleDashboardComponent } from '../../shared/components/sidenav-title-dashboard/sidenav-title-dashboard.component';
 import { ToolbarDashboardComponent } from '../../shared/components/toolbar-dashboard/toolbar-dashboard.component';
@@ -12,8 +24,148 @@ import { ToolbarDashboardComponent } from '../../shared/components/toolbar-dashb
 		SidenavTitleDashboardComponent,
 		SidenavDashboardComponent,
 		DashboardLayoutComponent,
+		ReactiveFormsModule,
+		FormInputComponent,
 	],
 	templateUrl: './dashboard-perfil.component.html',
 	styleUrl: './dashboard-perfil.component.css',
 })
-export class DashboardPerfilComponent {}
+export class DashboardPerfilComponent implements OnInit, OnDestroy {
+	private destroy$ = new Subject<void>();
+	loading = false; // Inicialmente, a página está carregando
+	@ViewChild('dialogContainer', { read: ViewContainerRef })
+	dialogContainer!: ViewContainerRef;
+
+	constructor(
+		private formBuilder: FormBuilder,
+		private cookieService: CookieService,
+		private router: Router
+	) {}
+
+	perfilForm = this.formBuilder.group({
+		name: ['', [Validators.required]],
+		email: ['', [Validators.required, Validators.email]],
+		phone: ['', [Validators.required]],
+		date: ['', [Validators.required]],
+		genre: ['', [Validators.required]],
+		bloodType: ['', [Validators.required]],
+		weight: ['', [Validators.required]],
+		height: ['', [Validators.required]],
+		needACaretaker: [false, [Validators.required]],
+	});
+
+	ngOnInit(): void {
+		this.perfilForm.reset();
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
+
+	onSubmitPerfilForm(): void {
+		this.loading = true;
+		// if (this.registerForm.value && this.registerForm.valid) {
+		// 	console.log(this.registerForm.value);
+		// 	this.accountService
+		// 		.registerAccount(this.registerForm.value as AccountRequest)
+		// 		.pipe(
+		// 			switchMap((response) => {
+		// 				this.cookieService.set(
+		// 					'ACCOUNT_ID',
+		// 					response.id.toString()
+		// 				);
+		// 				this.cookieService.set('ACCOUNT_EMAIL', response.email);
+		// 				this.emailAccount = response.email;
+		// 				return this.accountService.createTokenAccountActivate(
+		// 					this.emailAccount
+		// 				);
+		// 			}),
+		// 			takeUntil(this.destroy$)
+		// 		)
+		// 		.subscribe({
+		// 			next: (response) => {
+		// 				if (response) {
+		// 					this.registerForm.reset();
+		// 					this.loading = false;
+		// 					this.router.navigate(['/check-your-email']);
+		// 				}
+		// 			},
+		// 			complete: () => {
+		// 				this.loading = false;
+		// 			},
+		// 			error: (error) => {
+		// 				console.log(error);
+		// 				this.loading = false;
+		// 				switch (error.status) {
+		// 					case 401:
+		// 						this.openDialog(
+		// 							'Não Autorizado',
+		// 							'E-Mail ou senha inválido.',
+		// 							'Ok',
+		// 							'',
+		// 							() => {}
+		// 						);
+		// 						break;
+		// 					case 404:
+		// 						this.openDialog(
+		// 							'Email não cadastrado',
+		// 							'Favor verifique os dados informados e tente novamente.',
+		// 							'Ok',
+		// 							'',
+		// 							() => {}
+		// 						);
+		// 						break;
+		// 					case 500:
+		// 						this.openDialog(
+		// 							'Email já cadastrado',
+		// 							'Favor verifique os dados informados e tente novamente.',
+		// 							'Ok',
+		// 							'',
+		// 							() => {}
+		// 						);
+		// 						break;
+		// 					default:
+		// 						this.openDialog(
+		// 							'Ocorreu um erro',
+		// 							'Favor verifique os dados informados e tente novamente.',
+		// 							'Ok',
+		// 							'',
+		// 							() => {}
+		// 						);
+		// 						break;
+		// 				}
+		// 			},
+		// 		});
+		// }
+	}
+
+	openDialog(
+		title: string,
+		mensage: string,
+		buttonTextConfirm: string,
+		buttonTextClose?: any | undefined,
+		funcConfirmButton?: any | null
+	): void {
+		const componentRef =
+			this.dialogContainer.createComponent(DialogComponent);
+
+		componentRef.instance.data = {
+			title: title,
+			mensage: mensage,
+			buttonTextConfirm: buttonTextConfirm,
+			buttonTextClose: buttonTextClose,
+		};
+
+		componentRef.instance.close.subscribe(() => {
+			this.dialogContainer.clear(); // Fecha o diálogo
+		});
+
+		componentRef.instance.confirm.subscribe(() => {
+			// Ação a ser executada se o usuário clicar em "Continuar"
+			if (funcConfirmButton) {
+				funcConfirmButton();
+			}
+		});
+	}
+}
