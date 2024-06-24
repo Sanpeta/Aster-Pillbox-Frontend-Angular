c7085a816eb72f91aa315845f6fbcf4a555577f0
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-24 10:05:09 -0300

    feat: criado componente que vai mostrar a lista de medicamentos como nome, alarmes, quantidade e compartimentos.

---
+++ src/app/shared/components/list-medications/list-medications.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListMedicationsComponent } from './list-medications.component';
+
+describe('ListMedicationsComponent', () => {
+	let component: ListMedicationsComponent;
+	let fixture: ComponentFixture<ListMedicationsComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ListMedicationsComponent],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(ListMedicationsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
