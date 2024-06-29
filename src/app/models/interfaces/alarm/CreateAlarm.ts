d5cf591f920a85713a6b964697697fece220c7a5
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-06-28 23:30:55 -0300

    refactor: agora vai ser um array de string os alarms.

--- src/app/models/interfaces/alarm/CreateAlarm.ts
+++ src/app/models/interfaces/alarm/CreateAlarm.ts
@@ -1,9 +1,8 @@
 export interface CreateAlarmRequest {
-	time_alarm: string[];
+	time_alarms: string[];
 	is_active: boolean;
 	days_of_week: boolean[];
 	description: string;
-	create_at: string;
 }
 
 export interface CreateAlarmResponse {
