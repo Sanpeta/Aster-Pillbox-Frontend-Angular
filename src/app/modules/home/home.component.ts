982b840f118ca10227a9e0ab25f4b719dfb4a2c6
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-07-04 19:04:40 -0300

    feat: fazendo ancoragem com as sections da pagina.

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'app-home',
@@ -10,15 +10,49 @@ import { RouterModule } from '@angular/router';
 	styleUrl: './home.component.css',
 })
 export class HomeComponent {
+	@ViewChild('homeSection') homeSection: ElementRef = {} as ElementRef;
+	@ViewChild('functionsSection') functionsSection: ElementRef =
+		{} as ElementRef;
+	@ViewChild('plansSection') plansSection: ElementRef = {} as ElementRef;
+	@ViewChild('contactUsSection') contactUsSection: ElementRef =
+		{} as ElementRef;
+
 	sendMenssageForm = this.formBuilder.group({
 		name: '',
 		email: '',
 		message: '',
 	});
 
-	constructor(private formBuilder: FormBuilder) {}
+	constructor(private formBuilder: FormBuilder, private router: Router) {}
 
 	onSubmitMenssage() {
 		console.log(this.sendMenssageForm.value);
+	}
+
+	scrollToSection(sectionId: string) {
+		switch (sectionId) {
+			case 'home':
+				this.homeSection.nativeElement.scrollIntoView({
+					behavior: 'smooth',
+				});
+				break;
+			case 'functions':
+				this.functionsSection.nativeElement.scrollIntoView({
+					behavior: 'smooth',
+				});
+				break;
+			case 'plans':
+				this.plansSection.nativeElement.scrollIntoView({
+					behavior: 'smooth',
+				});
+				break;
+			case 'contact-us':
+				this.contactUsSection.nativeElement.scrollIntoView({
+					behavior: 'smooth',
+				});
+				break;
+			default:
+				break;
+		}
 	}
 }
