33ddb738eb129c49ad30fc7344d9baa9018ffbc5
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-07-04 02:16:15 -0300

    feat: criado o html da parte do fale conosco.

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [RouterModule],
+	imports: [RouterModule, ReactiveFormsModule],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+	sendMenssageForm = this.formBuilder.group({
+		name: '',
+		email: '',
+		message: '',
+	});
+
+	constructor(private formBuilder: FormBuilder) {}
+
+	onSubmitMenssage() {
+		console.log(this.sendMenssageForm.value);
+	}
+}
