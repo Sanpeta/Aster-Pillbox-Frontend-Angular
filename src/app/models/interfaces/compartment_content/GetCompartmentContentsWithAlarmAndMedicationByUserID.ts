f51834421007da9debd47d5ce99bfbc4d69a85b5
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-07-13 15:31:27 -0300

    fix: organizando as variaveis.

--- src/app/models/interfaces/compartment_content/GetCompartmentContentsWithAlarmAndMedicationByUserID.ts
+++ src/app/models/interfaces/compartment_content/GetCompartmentContentsWithAlarmAndMedicationByUserID.ts
@@ -3,17 +3,17 @@ export interface GetCompartmentContentsWithAlarmAndMedicationByUserIDRequest {
 }
 
 export interface GetCompartmentContentsWithAlarmAndMedicationByUserIDResponse {
-	compartment_id: number;
 	alarm_id: number;
+	case_id: number;
+	compartment_id: number;
 	medication_id: number;
-	time_alarms: string[];
-	is_active: boolean;
+	user_id: number;
 	days_of_week: boolean[];
-	index_compartment: number;
+	time_alarms: string[];
 	description: string;
 	name: string;
+	is_active: boolean;
+	index_compartment: number;
 	quantity_use_pill: number;
 	quantity_total_pill: number;
-	case_id: number;
-	user_id: number;
 }
