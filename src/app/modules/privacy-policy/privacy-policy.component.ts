12941d84bc517c85d0c5eb069fbb18a7ef9afec9
Sidnei de Souza Junior <sidneidesouzajunior@MacBook-Air-de-Sidnei.local>
2025-09-02 21:08:51 -0300

    refactor: feito ajustes a pagina de privacy policy.

--- src/app/modules/privacy-policy/privacy-policy.component.ts
+++ src/app/modules/privacy-policy/privacy-policy.component.ts
@@ -1,3 +1,4 @@
+// privacy-policy.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,6 @@ import { Component } from '@angular/core';
 	standalone: true,
 	imports: [],
 	templateUrl: './privacy-policy.component.html',
-	styleUrl: './privacy-policy.component.css',
+	styleUrls: ['./privacy-policy.component.css'],
 })
 export class PrivacyPolicyComponent {}
